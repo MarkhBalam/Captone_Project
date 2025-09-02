a3f6fd32e5114a9c87fb06e09bc63dd9a7191dfc
natashaaaliyah313 <natashaaaliyah313@gmail.com>
2025-09-02 17:28:14 +0300

    changed attributes capabilities to arrays

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Facility extends Model{
     protected $fillable = ['name','location','description','partner_organization','facility_type','capabilities'];
+    protected $casts = ['capabilities'=>'array'];
     
     
 }
