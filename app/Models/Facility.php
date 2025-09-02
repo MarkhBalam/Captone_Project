cbbefeef956ce1d0ca782858212aeb7f5278a307
natashaaaliyah313 <natashaaaliyah313@gmail.com>
2025-09-02 17:25:48 +0300

    Defined attributes for facility model

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Facility extends Model
-{
-    //
+class Facility extends Model{
+    protected $fillable = ['name','location','description','partner_organization','facility_type','capabilities'];
+    
+    
 }
