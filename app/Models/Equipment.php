765e72dc4919bd542d9b967964ae726166f63394
natashaaaliyah313 <natashaaaliyah313@gmail.com>
2025-09-02 18:01:14 +0300

    added reverse relationship with facility model

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipment extends Model{
     protected $fillable = ['facility_id','name','capabilities','description','inventory_code','usage_domain','support_phase'];
-    
+    public function facility() { return $this->belongsTo(Facility::class); }
     //
 }
