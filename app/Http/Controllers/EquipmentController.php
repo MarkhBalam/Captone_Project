206433cf404f5baf88e1e707d1913110868662c2
natashaaaliyah313 <natashaaaliyah313@gmail.com>
2025-09-02 19:13:40 +0300

    created facilityController class

---
+++ app/Http/Controllers/EquipmentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipment;
+use App\Models\Facility;
+use Illuminate\Http\Request;
+
+class EquipmentController extends Controller{
+    
+}
