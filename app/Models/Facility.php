b2c4640f90238b93909a77032b4f77ea39c705ce
natashaaaliyah313 <natashaaaliyah313@gmail.com>
2025-09-02 17:47:32 +0300

    added Equipment relationship to facility

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Facility extends Model{
     protected $fillable = ['name','location','description','partner_organization','facility_type','capabilities'];
     protected $casts = ['capabilities'=>'array'];
+    public function services() { return $this->hasMany(Service::class); }
+    public function equipment() { return $this->hasMany(Equipment::class); }
     
     
 }
