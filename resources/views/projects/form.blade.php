5539a19c92a1489fd874a74a433238f44b9e90cd
MarkhBalam <your-email@example.com>
2025-09-03 16:22:38 +0300

    added form.blade.php

---
+++ resources/views/projects/form.blade.php
@@ -0,0 +1,16 @@
+@php($edit = isset($project))
+@csrf
+<div class="row g-3">
+    <div class="col-md-6">
+        <label class="form-label">Title</label>
+        <input name="title" class="form-control" value="{{ old('title', $project->title ?? '') }}" required>
+    </div>
+    <div class="col-md-6">
+        <label class="form-label">Program</label>
+        <select name="program_id" class="form-select" required>
+            <option value="">Select program</option>
+            @foreach($programs as $pr)
+                <option value="{{ $pr->id }}" @selected(old('program_id', $project->program_id ?? '') == $pr->id)>{{ $pr->name }}</option>
+            @endforeach
+        </select>
+    </div>
