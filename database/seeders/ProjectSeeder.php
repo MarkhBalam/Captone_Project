b41613196499b7d306c942b3a298baa3340a3472
MarkhBalam <your-email@example.com>
2025-09-03 15:39:21 +0300

    added ProjectSeeder.php

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,3 +6,15 @@ use App\Models\{Project, Program, Facility};
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
+
+class ProjectSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $programIds   = Program::pluck('id')->all();
+        $facilityIds  = Facility::pluck('id')->all();
+
+        if (empty($programIds) || empty($facilityIds)) {
+            // Make sure ProgramSeeder and FacilitySeeder run first.
+            return;
+        }
