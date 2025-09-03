e5b4208312589e53539ceb1c12a89d624d3d4839
MarkhBalam <your-email@example.com>
2025-09-03 16:12:01 +0300

    added form.blade.php

---
+++ resources/views/projects/_form.blade.php
@@ -0,0 +1,9 @@
+﻿@php($edit = isset($project))
+@csrf
+<div class="row g-3">
+  <div class="col-md-6">
+    <label class="form-label">Title</label>
+    <input name="title" class="form-control @error('title') is-invalid @enderror"
+           value="{{ old('title', $project->title ?? '') }}" required>
+    @error('title') <div class="invalid-feedback">{{ $message }}</div> @enderror
+  </div>
