557c9b443f404d93edbb41bbcb17c1c8f6e029ee
MarkhBalam <your-email@example.com>
2025-09-03 15:37:15 +0300

    added ProjectSeeder.php

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\{Project, Program, Facility};
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
